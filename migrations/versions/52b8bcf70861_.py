4308f0367202f55c1963c02a52e11ac26f9d7048
TrofimTakoiOdin <arseniy.trofimov1993@gmail.com>
2024-02-15 09:24:54 +0300

    Commited migrations

---
+++ migrations/versions/52b8bcf70861_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 52b8bcf70861
+Revises: 
+Create Date: 2024-02-14 22:12:28.990221
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '52b8bcf70861'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
